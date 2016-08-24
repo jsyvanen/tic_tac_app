e26453157687a9a49f8cc640dc32a1ad36ad8872
Julius Syvanen <jsyvanen@rocketfuel.com>
2016-08-24 15:17:45 +0100

    Initial Commit

---
+++ tic_tac_engine.py
@@ -0,0 +1,103 @@
+from itertools import permutations as permuter
+from random import choice as rdm_choice
+
+class tac_game(object):
+    
+    winning_combos = [   # Rows
+                         ['a'+ str(z) for z in range(1,4)],
+                         ['b'+ str(z) for z in range(1,4)],
+                         ['c'+ str(z) for z in range(1,4)],
+                         # Columns
+                         [z + '1' for z in 'abc'],
+                         [z + '2' for z in 'abc'],
+                         [z + '3' for z in 'abc'],
+                         # Diagonal
+                         [z + str('abc'.index(z) + 1) for z in 'abc'],
+                         [z + str('cba'.index(z) + 1) for z in 'cba']   
+                     ]
+    
+    complete_board = [a + str(b) for a in 'abc' for b in range(1,4)]
+
+    def __init__(self,board = None):
+        self.board = board
+
+    def check_winner(self,board):
+        for player in board.keys():
+            board_state = list(permuter(board[player],3))
+            for pos_win_combo in board_state:
+                if list(pos_win_combo) in self.winning_combos:
+                    #print 'winner ',player
+                    return (player,1 if player == 'algo' else -1)
+                else:
+                    continue
+        if len(board['player']) + len(board['algo']) == len(self.complete_board):
+            return ('draw',0)
+        else:
+            return ('no',0)
+        
+
+    def get_move(self,opponent_type = None):
+        choice_set = self.get_choice_set(self.board)
+        if opponent_type == 'random':
+            if len(choice_set) > 0:
+                return rdm_choice(choice_set)
+            else:
+                return 'na'
+
+        elif opponent_type == 'minmax':
+            return self.iterate_moves(self.board,'algo')
+            
+    def get_choice_set(self,board):
+        #print [a for a in self.complete_board if a not in board['player'] and a not in board['algo']]
+        return [a for a in self.complete_board if a not in board['player'] and a not in board['algo']]
+
+    def augment_state(self,player,move,direction=''):
+        if direction == 'progress':
+            self.board[player].append(move)
+        else:
+            self.board[player].remove(move)
+    
+    def switch_player(self,player):
+        if player == 'algo':
+            return 'player'
+        else:
+            return 'algo'
+    
+    # NOT WORKING
+    def minimax(self,game_state,player):
+        game_winner,score = self.check_winner(game_state)
+        if game_winner in ('draw','algo','player'):
+            return score
+        if player == 'player':
+            best_move = float('-inf')
+        else:
+            best_move = float('inf')
+        choice_set = self.get_choice_set(game_state)
+        player = self.switch_player(player)
+        for move in choice_set:
+            self.augment_state(player,move,direction='progress')
+            opp_ret_val = self.minimax(game_state, player)
+            self.augment_state(player,move,direction='regress')
+            if player == 'algo':
+                if opp_ret_val > best_move:
+                    best_move = opp_ret_val
+            else:
+                if opp_ret_val < best_move:
+                    best_move = opp_ret_val
+            print player,' best move ',best_move
+            return best_move
+    
+    def iterate_moves(self,game_state,player):
+        game_winner,score = self.check_winner(game_state)
+        baseline_score = -2
+        choice_set = self.get_choice_set(game_state)
+        return_moves = []
+        for move in choice_set:
+            self.augment_state(player,move,direction='progress')
+            opp_ret_val = self.minimax(game_state,player)
+            self.augment_state(player,move,direction='regress')
+            if player == 'algo':
+                if opp_ret_val >= baseline_score:
+                    baseline_score = opp_ret_val
+                    return_moves.append(move)
+        return rdm_choice(return_moves)
---
+++ tic_tac_flask.py
@@ -0,0 +1,50 @@
+
+from flask import Flask,render_template,jsonify,request
+from tic_tac_engine import tac_game
+
+tac_app = Flask(__name__)
+
+
+board = {'player':[],
+		 'algo':[]
+		}
+
+@tac_app.route("/algo_play",methods = ['GET','POST'])
+def algo_play():
+
+	the_move = request.args.get('the_move')
+
+	if the_move in board['player'] or the_move in board['algo']:
+		return jsonify(error='occupied')
+
+	board['player'].append(the_move)
+	game_state = tac_game(board)
+	game_winner_player = game_state.check_winner(board)[0]
+	#print game_winner_player
+	if game_winner_player == 'player':
+		del board['player'][:]
+		del board['algo'][:]
+		return jsonify(winner = game_winner_player)
+
+	counter_move = game_state.get_move(opponent_type = 'random')
+	board["algo"].append(counter_move)
+	game_winner_algo = game_state.check_winner(board)[0]
+	print counter_move,game_winner_algo
+	if game_winner_algo == 'algo':
+		del board['player'][:]
+		del board['algo'][:]
+		return jsonify(winner = game_winner_algo,the_move = counter_move)
+
+	elif game_winner_algo == 'draw':
+		return jsonify(winner = 'draw')
+
+	else:
+		return jsonify(the_move = counter_move)
+		
+
+@tac_app.route("/")
+def main():
+	return render_template('tac_layout.html')
+
+if __name__ == '__main__':
+	tac_app.run()
